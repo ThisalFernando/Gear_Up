b441838398a85c9529633a6a7fef33930e1b0dda
thisal <thisalfernando39@gmail.com>
2025-03-09 13:52:50 +0530

    ReadOrderDetails.php was updated

--- ReadOrderDetails.php
+++ ReadOrderDetails.php
@@ -16,6 +16,102 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="Styles/productStyle.css">
     <link rel="stylesheet" href="Styles/cartStyle.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function confirmPayment() {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Do you want to procees payment?",
+                icon: 'question',
+                buttons: true,
+                dangerMode: true,
+                buttonsStyling: false,
+                showCancelButton: true,
+                confirmButtonText: 'Yes, Proceed!',
+                cancelButtonText: 'Cancel', 
+                buttonsStyling: false,// Disable default SweetAlert2 button styling
+                didRender: () => {
+                const confirmButton = Swal.getConfirmButton();
+                const cancelButton = Swal.getCancelButton();
+            
+                // Apply styles directly using JavaScript
+                confirmButton.style.backgroundColor = '#015f30';
+                confirmButton.style.color = 'white';
+                confirmButton.style.width = '450px';
+                confirmButton.style.border = 'none';
+                confirmButton.style.borderRadius = '5px';
+                confirmButton.style.padding = '10px';
+                confirmButton.style.fontWeight = 'bold';
+                confirmButton.style.fontSize = 'medium';
+                confirmButton.style.cursor = 'pointer';
+
+                cancelButton.style.backgroundColor = 'red'; 
+                cancelButton.style.color = 'white';
+                cancelButton.style.width = '450px';
+                cancelButton.style.border = 'none';
+                cancelButton.style.borderRadius = '5px';
+                cancelButton.style.padding = '10px';
+                cancelButton.style.fontWeight = 'bold';
+                cancelButton.style.fontSize = 'medium';
+                cancelButton.style.cursor = 'pointer'
+                cancelButton.style.marginTop = '10px';
+                },
+            })
+            .then((result) =>{
+                if(result.isConfirmed){
+                    window.location.href = '#';
+                }
+            });
+
+            return false; 
+        }
+        function confirmShopping() {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "Do you want to continue shopping?",
+                icon: 'question',
+                buttons: true,
+                dangerMode: true,
+                buttonsStyling: false,
+                showCancelButton: true,
+                confirmButtonText: 'Yes, Continue!',
+                cancelButtonText: 'Cancel', 
+                buttonsStyling: false,// Disable default SweetAlert2 button styling
+                didRender: () => {
+                const confirmButton = Swal.getConfirmButton();
+                const cancelButton = Swal.getCancelButton();
+            
+                // Apply styles directly using JavaScript
+                confirmButton.style.backgroundColor = '#015f30';
+                confirmButton.style.color = 'white';
+                confirmButton.style.width = '450px';
+                confirmButton.style.border = 'none';
+                confirmButton.style.borderRadius = '5px';
+                confirmButton.style.padding = '10px';
+                confirmButton.style.fontWeight = 'bold';
+                confirmButton.style.fontSize = 'medium';
+                confirmButton.style.cursor = 'pointer';
+
+                cancelButton.style.backgroundColor = 'red'; 
+                cancelButton.style.color = 'white';
+                cancelButton.style.width = '450px';
+                cancelButton.style.border = 'none';
+                cancelButton.style.borderRadius = '5px';
+                cancelButton.style.padding = '10px';
+                cancelButton.style.fontWeight = 'bold';
+                cancelButton.style.fontSize = 'medium';
+                cancelButton.style.cursor = 'pointer'
+                cancelButton.style.marginTop = '10px';
+                },
+            })
+            .then((result) =>{
+                if(result.isConfirmed){
+                    window.location.href = 'Products.php';
+                }
+            });
+
+            return false; 
+        }
+    </script>
 </head>
 <body>
     <nav>
@@ -63,9 +159,9 @@ $result = $conn->query($sql);
             <?php } ?>
             
         </table><br><br>
-        <a href="Products.php"><button id="contBtn">CONTINUE SHOPPING</button></a>
+        <button id="contBtn" onclick="confirmShopping()">CONTINUE SHOPPING</button>
         <br><br>
-        <a href="#"><button id="contBtn">CONFIRM PAYMENT</button></a>
+        <a href="#"><button id="contBtn" onclick="confirmPayment()">CONFIRM PAYMENT</button></a>
         <br><br><br><br><br>
         <div class="end_text"><b>THANK YOU FOR CHOOSING US!</b></div>
         <br><br><br><br><br><br>
